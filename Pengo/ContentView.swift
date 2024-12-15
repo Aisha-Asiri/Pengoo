a3f6925a2c1e3fa0833c4a8eb452f0103c2549aa
Aisha-Asiri <travel2paradise2@gmail.com>
2024-12-15 12:21:12 +0300

    Initial Commit

---
+++ Pengo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pengo
+//
+//  Created by Aisha Asiri on 14/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pengo/PengoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PengoApp.swift
+//  Pengo
+//
+//  Created by Aisha Asiri on 14/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PengoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
